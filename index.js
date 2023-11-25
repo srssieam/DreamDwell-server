3dd0aa5f057756510106443fd12dd65d156fd7da
srs-sieam <srssieam@gmail.com>
2023-11-25 14:13:13 +0600

    basic server setup done

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Dream Dwell server is running')
+})
+
+app.listen(PORT, ()=> {
+    console.log(`DreamDwell server is running on http://localhost:${PORT}`)
+})
