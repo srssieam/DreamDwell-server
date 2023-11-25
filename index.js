59d411617b6e973ca21e440e9bf028e19ef42fbd
srs-sieam <srssieam@gmail.com>
2023-11-25 19:17:01 +0600

    get All Users Info form database and send to client side

--- index.js
+++ index.js
@@ -51,6 +51,11 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/v1/api/users', async(req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
