ac18369b0bdbcf37d01a9d825f6973c4bcf901f4
srs-sieam <srssieam@gmail.com>
2023-11-26 08:26:40 +0600

    Update User to admin role:- filter user using id and set updatedDoc {role:admin}

--- index.js
+++ index.js
@@ -63,6 +63,19 @@ async function run() {
       res.send(result);
     })
 
+    // update user to admin
+    app.patch('/v1/api/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc)
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
