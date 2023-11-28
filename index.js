ac51187ffd7018e8b5323916f4636f9b03d25e60
srs-sieam <srssieam@gmail.com>
2023-11-28 22:31:16 +0600

    get review for spacific property

--- index.js
+++ index.js
@@ -39,12 +39,20 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/v1/api/reviews', async(req, res) => {
+      const title = req.query.propertyTitle;
+      const query = { property_title: title };
+      const result = await reviewsCollection.find(query).toArray();
+      res.send(result);
+    })
+
     app.delete('/v1/api/reviews/:id', async(req, res) => {
       const id = req.params.id ;
       const query = { _id: new ObjectId(id) };
       const result = await reviewsCollection.deleteOne(query);
       res.send(result);
     })
+
 
     // user related api
     app.post('/v1/api/users', async(req, res) => {
