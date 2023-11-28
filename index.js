ce3ce810eb7a2a9b7f13a073af5da587fa54370c
srs-sieam <srssieam@gmail.com>
2023-11-28 18:39:15 +0600

    delete property from wishlist

--- index.js
+++ index.js
@@ -247,6 +247,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/v1/api/wishlist/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await wishlistCollection.deleteOne(query)
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
