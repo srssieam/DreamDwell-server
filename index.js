779ddaff91357a28ce3a0f200a1679cdbc891aab
srs-sieam <srssieam@gmail.com>
2023-11-29 18:56:36 +0600

    complete verify token process

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
@@ -10,11 +11,30 @@ const PORT = process.env.PORT || 5000;
 // middleware
 app.use(cors({
   origin: [
-      'http://localhost:5173' 
+    'http://localhost:5173'
   ],
   credentials: true
 }));
 app.use(express.json())
+app.use(cookieParser());
+
+// middleware for user verification
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.dreamDwell;   // get the cookie from client site
+  console.log('token in the middleware', token);
+  // if no token available
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+  // if token available 
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: 'unauthorized access' })
+    }
+    req.user = decoded; // get the user who have token
+    next();
+  })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xjpiwvy.mongodb.net/?retryWrites=true&w=majority`;
@@ -52,47 +72,47 @@ async function run() {
         httpOnly: true,
         secure: true,
         sameSite: 'none'
-      	})
-      res.send({success: true});
+      })
+      res.send({ success: true });
     })
 
     // delete the  cookie in client site
-    app.post('/logout', async(req, res) =>{
+    app.post('/logout', async (req, res) => {
       const loggedUser = req.body;  // get loggedUser={}
       console.log('logging out', loggedUser)
       res.clearCookie('dreamDwell', { maxAge: 0 }).send({ success: true }) // clear the cookie
-  })
+    })
 
 
 
     // reviews related api
-    app.get('/v1/api/reviews', async(req, res) => {
+    app.get('/v1/api/reviews', async (req, res) => {
       const title = req.query.propertyTitle;
-      if(title){
+      if (title) {
         const query = { property_title: title };
-        const result = await reviewsCollection.find(query).sort( { _id : -1} ).toArray();
+        const result = await reviewsCollection.find(query).sort({ _id: -1 }).toArray();
         res.send(result);
         return;
       }
 
-      const result = await reviewsCollection.find().sort( { _id : -1} ).toArray();
+      const result = await reviewsCollection.find().sort({ _id: -1 }).toArray();
       res.send(result);
     })
 
-    app.get('/v1/api/myReviews', async(req, res) => {
+    app.get('/v1/api/myReviews', async (req, res) => {
       const name = req.query.reviewerName;
-      const result = await reviewsCollection.find({reviewer_name: name}).sort( { _id : -1} ).toArray();
+      const result = await reviewsCollection.find({ reviewer_name: name }).sort({ _id: -1 }).toArray();
       res.send(result)
     })
 
-    app.delete('/v1/api/reviews/:id', async(req, res) => {
-      const id = req.params.id ;
+    app.delete('/v1/api/reviews/:id', verifyToken, async (req, res) => {
+      const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await reviewsCollection.deleteOne(query);
       res.send(result);
     })
 
-    app.post('/v1/api/reviews', async(req, res) => {
+    app.post('/v1/api/reviews', async (req, res) => {
       const newReview = req.body;
       const result = await reviewsCollection.insertOne(newReview);
       res.send(result)
@@ -100,13 +120,13 @@ async function run() {
 
 
     // user related api
-    app.post('/v1/api/users', async(req, res) => {
+    app.post('/v1/api/users', async (req, res) => {
       const user = req.body;
-  
+
       // don't insert userInfo if user already exists
-      const query = {email: user.email}
+      const query = { email: user.email }
       const existingUser = await usersCollection.findOne(query); // find user with the existing email
-      if(existingUser){
+      if (existingUser) {
         return res.send({ message: 'user already exists', insertedId: null });
       }
 
@@ -115,12 +135,12 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/v1/api/users', async(req, res) => {
+    app.get('/v1/api/users', verifyToken, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     })
 
-    app.delete('/v1/api/users/:id', async(req, res) => {
+    app.delete('/v1/api/users/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
       console.log(id);
       const result = await usersCollection.deleteOne({ _id: new ObjectId(id) });
@@ -128,7 +148,7 @@ async function run() {
     })
 
     // update user to admin
-    app.patch('/v1/api/users/admin/:id', async (req, res) => {
+    app.patch('/v1/api/users/admin/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
@@ -141,7 +161,7 @@ async function run() {
     })
 
     // update user to agent
-    app.patch('/v1/api/users/agent/:id', async (req, res) => {
+    app.patch('/v1/api/users/agent/:id',verifyToken, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
@@ -154,7 +174,7 @@ async function run() {
     })
 
     // mark as fraud
-    app.patch('/v1/api/users/fraud/:id', async (req, res) => {
+    app.patch('/v1/api/users/fraud/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
@@ -173,52 +193,52 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/v1/api/agentAddedProperties', async (req, res) => {
-        const email = req.query.email;
-        const query = { agent_email: email };
-        const result = await propertyCollection.find(query).toArray();
-        res.send(result);
+    app.get('/v1/api/agentAddedProperties', verifyToken, async (req, res) => {
+      const email = req.query.email;
+      const query = { agent_email: email };
+      const result = await propertyCollection.find(query).toArray();
+      res.send(result);
     })
 
-    app.delete('/v1/api/agentAddedProperties/:id', async (req, res) => {
-        const id = req.params.id
-        const query = { _id: new ObjectId(id) };
-        const result = await propertyCollection.deleteOne(query);
-        res.send(result);
+    app.delete('/v1/api/agentAddedProperties/:id', verifyToken, async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) };
+      const result = await propertyCollection.deleteOne(query);
+      res.send(result);
     })
 
     app.get('/v1/api/agentAddedProperties/:id', async (req, res) => {
-        const id = req.params.id
-        const query = { _id: new ObjectId(id) };
-        const result = await propertyCollection.findOne(query);
-        res.send(result);
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) };
+      const result = await propertyCollection.findOne(query);
+      res.send(result);
     })
 
-    app.patch('/v1/api/agentAddedProperties/:id', async (req, res) => {
-        const id = req.params.id;
-        // console.log(id)
-        const property = req.body;
-        // console.log(property)
-        const filter = { _id: new ObjectId(id) };
-        const updateDoc = {
-          $set: property     
-        }
-        const result = await propertyCollection.updateOne(filter,updateDoc);
-        res.send(result);
+    app.patch('/v1/api/agentAddedProperties/:id', verifyToken, async (req, res) => {
+      const id = req.params.id;
+      // console.log(id)
+      const property = req.body;
+      // console.log(property)
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: property
+      }
+      const result = await propertyCollection.updateOne(filter, updateDoc);
+      res.send(result);
     })
 
-    app.post('/v1/api/agentAddedProperties', async (req, res) => {
-        const newProperty = req.body;
-        const result = await propertyCollection.insertOne(newProperty);
-        res.send(result);
+    app.post('/v1/api/agentAddedProperties', verifyToken, async (req, res) => {
+      const newProperty = req.body;
+      const result = await propertyCollection.insertOne(newProperty);
+      res.send(result);
     })
 
     app.get('/v1/api/allVerifiedProperties', async (req, res) => {
-      if(req.query?.search){
+      if (req.query?.search) {
         const filter = req.query.search;
         // console.log(filter);
         const query = {
-          property_title: {$regex: filter, $options: 'i'}
+          property_title: { $regex: filter, $options: 'i' }
         };
         const result = await propertyCollection.find(query).toArray();
         res.send(result);
@@ -231,17 +251,17 @@ async function run() {
 
     app.get('/v1/api/allVerifiedProperties/:id', async (req, res) => {
       const query = { _id: new ObjectId(req.params.id) };
-      res.send( await propertyCollection.findOne(query) );
+      res.send(await propertyCollection.findOne(query));
     })
 
-    app.post('/v1/api/advertisement', async (req, res) => {
+    app.post('/v1/api/advertisement', verifyToken, async (req, res) => {
       const advertisement = req.body;
       // console.log(advertisement)
       const result = await advertisementCollection.insertOne(advertisement);
       res.send(result)
     })
 
-    app.delete('/v1/api/advertisement/:id', async (req, res) => {
+    app.delete('/v1/api/advertisement/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
       console.log(id);
       const query = { _id: id };
@@ -249,19 +269,19 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/v1/api/advertisement', async (req, res)=> {
+    app.get('/v1/api/advertisement', async (req, res) => {
       const result = await advertisementCollection.find().toArray()
       res.send(result)
     })
 
-    app.delete('v1/api/properties/:id', async (req, res) => {
+    app.delete('v1/api/properties/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await propertyCollection.deleteOne(query);
       res.send(result)
     })
 
-    app.patch('/v1/api/properties/verify/:id', async (req, res) => {
+    app.patch('/v1/api/properties/verify/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) }
       const updatedDoc = {
@@ -269,11 +289,11 @@ async function run() {
           verification_status: "verified"
         }
       }
-      const result = await propertyCollection.updateOne(filter,  updatedDoc)
+      const result = await propertyCollection.updateOne(filter, updatedDoc)
       res.send(result);
     })
 
-    app.patch('/v1/api/properties/reject/:id', async (req, res) => {
+    app.patch('/v1/api/properties/reject/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) }
       const updatedDoc = {
@@ -281,11 +301,11 @@ async function run() {
           verification_status: "rejected"
         }
       }
-      const result = await propertyCollection.updateOne(filter,  updatedDoc)
+      const result = await propertyCollection.updateOne(filter, updatedDoc)
       res.send(result);
     })
 
-    app.delete('/v1/api/fraudProperty/:email', async (req,res)=>{
+    app.delete('/v1/api/fraudProperty/:email', verifyToken, async (req, res) => {
       const fraudUser = req.params.email;
       // console.log(fraudUser)
       const query = { agent_email: fraudUser }
@@ -302,15 +322,20 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/v1/api/wishlist', async (req, res) => {
-      const  email= req.query.email;
+    app.get('/v1/api/wishlist', verifyToken, async (req, res) => {
+      const email = req.query.email;
       // console.log(email);
-      const query = {buyerEmail: email}
+      // console.log('cookies from client site', req.cookies)  // get cookies from client site
+      // console.log('token owner info', req.user.email)
+      if(req.user.email !== req.query.email){  // compare between user email and email in data base
+        return res.status(403).send({message: 'forbidden access'})
+      }
+      const query = { buyerEmail: email }
       const result = await wishlistCollection.find(query).toArray();
       res.send(result);
     })
 
-    app.delete('/v1/api/wishlist/:id', async (req, res) => {
+    app.delete('/v1/api/wishlist/:id',verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: id }
       const result = await wishlistCollection.deleteOne(query)
@@ -319,18 +344,18 @@ async function run() {
 
     app.get('/v1/api/wishlist/:id', async (req, res) => {
       const query = { _id: req.params.id };
-      res.send( await wishlistCollection.findOne(query) );
+      res.send(await wishlistCollection.findOne(query));
     })
 
     // offered properties
-    app.post('/v1/api/allOfferedProperties', async (req, res) => {
+    app.post('/v1/api/allOfferedProperties', verifyToken, async (req, res) => {
       const offeredProperty = req.body;
       // console.log(wishlistProperty)
       const result = await offeredCollection.insertOne(offeredProperty);
       res.send(result);
     })
 
-    app.get('/v1/api/usersOfferedProperties', async (req, res) => {
+    app.get('/v1/api/usersOfferedProperties', verifyToken, async (req, res) => {
       const email = req.query.email;
       const query = { buyer_email: email }
       const result = await offeredCollection.find(query).toArray();
@@ -344,15 +369,15 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/v1/api/allOfferedProperties', async (req, res) => {
+    app.get('/v1/api/allOfferedProperties', verifyToken, async (req, res) => {
       const email = req.query.email;
       const query = { buyer_email: email }
       const result = await offeredCollection.find(query).toArray();
       res.send(result);
     })
 
-    
-    app.patch('/v1/api/allOfferedProperties/paid/:id', async (req, res) => {
+
+    app.patch('/v1/api/allOfferedProperties/paid/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: id }
       const updatedDoc = {
@@ -361,10 +386,10 @@ async function run() {
         }
       }
       const result = await offeredCollection.updateOne(filter, updatedDoc)
-      res.send(result); 
+      res.send(result);
     })
 
-    app.patch('/v1/api/offeredProperties/accept/:id', async (req, res) => {
+    app.patch('/v1/api/offeredProperties/accept/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: id }
       const updatedDoc = {
@@ -376,7 +401,7 @@ async function run() {
       res.send(result);
     })
 
-    app.patch('/v1/api/offeredProperties/reject/:id', async (req, res) => {
+    app.patch('/v1/api/offeredProperties/reject/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: id }
       const updatedDoc = {
@@ -390,20 +415,20 @@ async function run() {
 
 
     // payment intent
-    app.post('/v1/api/create-payment-intent', async(req, res) => {
-      const {price} = req.body;  // get price from client
+    app.post('/v1/api/create-payment-intent', async (req, res) => {
+      const { price } = req.body;  // get price from client
       console.log(price)
       const amount = parseInt(price * 100); // convert tk into poisa
       const paymentIntent = await stripe.paymentIntents.create({
-          amount: amount,
-          currency: 'usd',
-          payment_method_types: ['card']
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
       })
 
       res.send({
-          clientSecret: paymentIntent.client_secret
+        clientSecret: paymentIntent.client_secret
       })
-  })
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -417,9 +442,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Dream Dwell server is running')
+  res.send('Dream Dwell server is running')
 })
 
-app.listen(PORT, ()=> {
-    console.log(`DreamDwell server is running on http://localhost:${PORT}`)
+app.listen(PORT, () => {
+  console.log(`DreamDwell server is running on http://localhost:${PORT}`)
 })
